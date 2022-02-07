d39cc989c3a0e1a307bcdf258468b18cbcf66ac2
igortakeo <igor.takeo@usp.br>
2022-02-07 14:57:19 -0300

    Add maven project

---
+++ src/main/java/com/bookingflight/bookingflight/BookingflightApplication.java
@@ -0,0 +1,13 @@
+package com.bookingflight.bookingflight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingflightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingflightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookingflight/bookingflight/BookingflightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookingflight.bookingflight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingflightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
