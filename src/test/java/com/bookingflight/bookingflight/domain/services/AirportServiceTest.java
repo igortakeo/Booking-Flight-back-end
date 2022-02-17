a4f63d03fdf1a6189690a6ce22d87f3e70acad1b
igortakeo <igor.takeo@usp.br>
2022-02-17 14:34:42 -0300

    Finish tests to airport service

--- src/test/java/com/bookingflight/bookingflight/domain/services/AirportServiceTest.java
+++ src/test/java/com/bookingflight/bookingflight/domain/services/AirportServiceTest.java
@@ -1,5 +1,6 @@
 package com.bookingflight.bookingflight.domain.services;
 
+import com.bookingflight.bookingflight.domain.Airline;
 import com.bookingflight.bookingflight.domain.Airport;
 import com.bookingflight.bookingflight.domain.services.exceptions.ObjectAlreadyExistException;
 import com.bookingflight.bookingflight.domain.services.exceptions.ObjectNotFoundException;
@@ -16,7 +17,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
@@ -135,17 +137,138 @@ class AirportServiceTest {
 
     @Test
     void update() {
+        final Airport airportTest = new Airport(
+                null,
+                "airportTest",
+                "streetTest",
+                10,
+                "cepTest",
+                "cityTest"
+        );
+
+        final Airport airportTestUpdate = new Airport(
+                null,
+                "airportTest1",
+                "streetTest1",
+                11,
+                "cepTest1",
+                "cityTest1"
+        );
+
+        airportService.create(airportTest);
+
+        airportService.update(airportTest.getId(), airportTestUpdate);
+
+        Airport airportReturnTest = airportService.findById(airportTest.getId());
+
+        assertEquals(airportTestUpdate.getName(), airportReturnTest.getName());
+        assertEquals(airportTestUpdate.getStreet(), airportReturnTest.getStreet());
+        assertEquals(airportTestUpdate.getNumber(), airportReturnTest.getNumber());
+        assertEquals(airportTestUpdate.getCep(), airportReturnTest.getCep());
+        assertEquals(airportTestUpdate.getCity(), airportReturnTest.getCity());
+    }
+
+    @Test
+    void updateAirportAlreadyExist(){
+        final Airport airportTest1 = new Airport(
+                null,
+                "airportTest1",
+                "streetTest1",
+                10,
+                "cepTest1",
+                "cityTest1"
+        );
+
+        final Airport airportTest2 = new Airport(
+                null,
+                "airportTest2",
+                "streetTest2",
+                10,
+                "cepTest2",
+                "cityTest2"
+        );
+
+        final Airport airportTestUpdate = new Airport(
+                null,
+                "airportTest1",
+                "streetTest3",
+                10,
+                "cepTest3",
+                "cityTest3"
+        );
+
+        airportService.create(airportTest1);
+
+        airportService.create(airportTest2);
+
+        assertThrows(ObjectAlreadyExistException.class, () -> airportService.update(airportTest2.getId(), airportTestUpdate));
     }
 
     @Test
     void delete() {
+        final Airport airportTest = new Airport(
+                null,
+                "airportTest",
+                "streetTest",
+                10,
+                "cepTest",
+                "cityTest"
+        );
+
+        airportService.create(airportTest);
+
+        airportService.delete(airportTest.getId());
+
+        assertThrows(ObjectNotFoundException.class, () -> airportService.findById(airportTest.getId()));
     }
 
-    @Test
-    void findAirlines() {
-    }
 
     @Test
-    void addAirline() {
+    void addAirlinesAndFindAirlines() {
+        final Airport airportTest = new Airport(
+                null,
+                "airportTest",
+                "streetTest",
+                10,
+                "cepTest",
+                "cityTest"
+        );
+
+        final Airline airlineTest1 = new Airline(
+                "codeTest1",
+                "nameTest1",
+                10,
+                "test@gmail.com",
+                "79984133545"
+        );
+
+        final Airline airlineTest2 = new Airline(
+                "codeTest2",
+                "nameTest2",
+                112,
+                "test@gmail.com",
+                "79984133545"
+        );
+
+        final List<Airline> airlineTestList = new ArrayList<>(Arrays.asList(
+                airlineTest1,
+                airlineTest2
+        ));
+
+        airportService.create(airportTest);
+
+        airlineRepository.saveAll(airlineTestList);
+
+        airportService.addAirline(airportTest.getId(), airlineTest1.getCode());
+
+        airportService.addAirline(airportTest.getId(), airlineTest2.getCode());
+
+        List<Airline> airlineReturnList = airportService.findAirlines(airportTest.getId());
+
+        assertEquals(airlineTestList.size(), airlineReturnList.size());
+
+        for(int i=0; i<airlineReturnList.size(); i++){
+            assertEquals(airlineTestList.get(i), airlineReturnList.get(i));
+        }
     }
 }
